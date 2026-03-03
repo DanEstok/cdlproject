fb5d2d51cd9d6f4c2d9b3f1297e7fb8b5f734780
DanEstok <destok001@my.wilmu.edu>
2026-03-02 19:29:59 -0500

    Remove API distribution files from version control

---
+++ apps/api/dist/src/auth/clerk.strategy.d.ts
@@ -0,0 +1,24 @@
+import { Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { PrismaService } from '../common/prisma/prisma.service';
+declare const ClerkStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
+    validate(...args: any[]): unknown;
+};
+export declare class ClerkStrategy extends ClerkStrategy_base {
+    private configService;
+    private prisma;
+    constructor(configService: ConfigService, prisma: PrismaService);
+    validate(payload: any): Promise<{
+        userId: string;
+        clerkUserId: string;
+        role: import("@prisma/client").$Enums.UserRole;
+        organizationId: string;
+        organization: {
+            id: string;
+            name: string;
+            createdAt: Date;
+            updatedAt: Date;
+        };
+    }>;
+}
+export {};
